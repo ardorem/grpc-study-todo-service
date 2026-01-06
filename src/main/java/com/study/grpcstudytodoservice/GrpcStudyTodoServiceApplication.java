9698d331bfae384b5991c6849059a10d0b2646a0
Miles Moon <miles.moon@bluedus.co.kr>
2026-01-06 14:36:05 +0900

    init

---
+++ src/main/java/com/study/grpcstudytodoservice/GrpcStudyTodoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.study.grpcstudytodoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcStudyTodoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcStudyTodoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/grpcstudytodoservice/GrpcStudyTodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.grpcstudytodoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcStudyTodoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
